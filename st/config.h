db801a71962474cb6db99d9568a47d8656d4704a
unknown <unknown@unkno.wn>
2023-09-21 14:19:56 -0300

    Remove useless files

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Inconsolata Nerd Font:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -180,8 +180,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ ShiftMask,            XK_K,           kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_J,           kscrolldown,    {.i = -1} },
+	/* { ShiftMask,            XK_K,           kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_J,           kscrolldown,    {.i = -1} }, */
 };
 
 /*
