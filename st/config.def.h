359c0d3fe69b50d70ad3db6623eb9749be3590b5
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-03-26 12:17:17 -0300

    add padding to st

--- st/config.def.h
+++ st/config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono Nerd Font:pixelsize=19";
-static int borderpx = 2;
+static int borderpx = 6;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 #include "/home/unknown/.cache/wal/colors-wal-st.h"
